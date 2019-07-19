82099e4b8f7f11506e3430601e27e2b75f41960f
sudhirsardhara <51163456+sudhirsardhara@users.noreply.github.com>
2019-07-19 15:56:57 +0530

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -16,14 +16,7 @@ EXIT_COLOR = (66, 183, 42)
 
 
 class PipelineRunner(object):
-    '''
-        Very simple pipline.
-
-        Just run passed processors in order with passing context from one to 
-        another.
-
-        You can also set log level for processors.
-    '''
+    
 
     def __init__(self, pipeline=None, log_level=logging.DEBUG):
         self.pipeline = pipeline or []
@@ -64,28 +57,13 @@ class PipelineRunner(object):
 
 
 class PipelineProcessor(object):
-    '''
-        Base class for processors.
-    '''
-
+    
     def __init__(self):
         self.log = logging.getLogger(self.__class__.__name__)
 
 
 class ContourDetection(PipelineProcessor):
-    '''
-        Detecting moving objects.
-
-        Purpose of this processor is to subtrac background, get moving objects
-        and detect them with a cv2.findContours method, and then filter off-by
-        width and height. 
-
-        bg_subtractor - background subtractor isinstance.
-        min_contour_width - min bounding rectangle width.
-        min_contour_height - min bounding rectangle height.
-        save_image - if True will save detected objects mask to file.
-        image_dir - where to save images(must exist).        
-    '''
+   
 
     def __init__(self, bg_subtractor, min_contour_width=35, min_contour_height=35, save_image=False, image_dir='images'):
         super(ContourDetection, self).__init__()
@@ -97,18 +75,15 @@ class ContourDetection(PipelineProcessor):
         self.image_dir = image_dir
 
     def filter_mask(self, img, a=None):
-        '''
-            This filters are hand-picked just based on visual tests
-        '''
-
+       
         kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 2))
 
-        # Fill any small holes
+        
         closing = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
-        # Remove noise
+       
         opening = cv2.morphologyEx(closing, cv2.MORPH_OPEN, kernel)
 
-        # Dilate to merge adjacent blobs
+        
         dilation = cv2.dilate(opening, kernel, iterations=2)
 
         return dilation
@@ -117,7 +92,7 @@ class ContourDetection(PipelineProcessor):
 
         matches = []
 
-        # finding external contours
+      
         im2, contours, hierarchy = cv2.findContours(
             fg_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_TC89_L1)
 
@@ -140,7 +115,7 @@ class ContourDetection(PipelineProcessor):
         frame_number = context['frame_number']
 
         fg_mask = self.bg_subtractor.apply(frame, None, 0.001)
-        # just thresholding values
+        
         fg_mask[fg_mask < 240] = 0
         fg_mask = self.filter_mask(fg_mask, frame_number)
 
@@ -155,16 +130,7 @@ class ContourDetection(PipelineProcessor):
 
 
 class VehicleCounter(PipelineProcessor):
-    '''
-        Counting vehicles that entered in exit zone.
-
-        Purpose of this class based on detected object and local cache create
-        objects pathes and count that entered in exit zone defined by exit masks.
-
-        exit_masks - list of the exit masks.
-        path_size - max number of points in a path.
-        max_dst - max distance between two points.
-    '''
+    
 
     def __init__(self, exit_masks=[], path_size=10, max_dst=30, x_weight=1.0, y_weight=1.0):
         super(VehicleCounter, self).__init__()
@@ -198,14 +164,13 @@ class VehicleCounter(PipelineProcessor):
         points = np.array(objects)[:, 0:2]
         points = points.tolist()
 
-        # add new points if pathes is empty
+       
         if not self.pathes:
             for match in points:
                 self.pathes.append([match])
 
         else:
-            # link new points with old pathes based on minimum distance between
-            # points
+           
             new_pathes = []
 
             for path in self.pathes:
@@ -213,11 +178,10 @@ class VehicleCounter(PipelineProcessor):
                 _match = None
                 for p in points:
                     if len(path) == 1:
-                        # distance from last point to current
+                        
                         d = utils.distance(p[0], path[-1][0])
                     else:
-                        # based on 2 prev points predict next point and calculate
-                        # distance from predicted next point to current
+                        
                         xn = 2 * path[-1][0][0] - path[-2][0][0]
                         yn = 2 * path[-1][0][1] - path[-2][0][1]
                         d = utils.distance(
@@ -235,13 +199,13 @@ class VehicleCounter(PipelineProcessor):
                     path.append(_match)
                     new_pathes.append(path)
 
-                # do not drop path if current frame has no matches
+                
                 if _match is None:
                     new_pathes.append(path)
 
             self.pathes = new_pathes
 
-            # add new pathes
+            
             if len(points):
                 for p in points:
                     # do not add points that already should be counted
@@ -249,28 +213,28 @@ class VehicleCounter(PipelineProcessor):
                         continue
                     self.pathes.append([p])
 
-        # save only last N points in path
+        
         for i, _ in enumerate(self.pathes):
             self.pathes[i] = self.pathes[i][self.path_size * -1:]
 
-        # count vehicles and drop counted pathes:
+       
         new_pathes = []
         for i, path in enumerate(self.pathes):
             d = path[-2:]
 
             if (
-                # need at list two points to count
+                
                 len(d) >= 2 and
-                # prev point not in exit zone
+              
                 not self.check_exit(d[0][1]) and
-                # current point in exit zone
+               # drawing top block with counts
                 self.check_exit(d[1][1]) and
-                # path len is bigger then min
+                
                 self.path_size <= len(path)
             ):
                 self.vehicle_count += 1
             else:
-                # prevent linking with path that already in exit zone
+                
                 add = True
                 for p in path:
                     if self.check_exit(p[1]):
@@ -362,14 +326,14 @@ class Visualizer(PipelineProcessor):
 
     def draw_ui(self, img, vehicle_count, exit_masks=[]):
 
-        # this just add green mask with opacity to the image
+        
         for exit_mask in exit_masks:
             _img = np.zeros(img.shape, img.dtype)
             _img[:, :] = EXIT_COLOR
             mask = cv2.bitwise_and(_img, _img, mask=exit_mask)
             cv2.addWeighted(mask, 1, img, 1, 0, img)
 
-        # drawing top block with counts
+        
         cv2.rectangle(img, (0, 0), (img.shape[1], 50), (0, 0, 0), cv2.FILLED)
         cv2.putText(img, ("Vehicles passed: {total} ".format(total=vehicle_count)), (30, 30),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 1)
